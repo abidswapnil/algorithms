4fad6dabf61009ea9fff0424039b70ccf036d579
abid <swapnil1510@cseku.ac.bd>
2023-08-08 17:02:45 +0600

    commented bubblesort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,13 +1,26 @@
 def bubbleSort(nums):
-  l = len(nums)
-  for i in range(l):
-    swapped = False
-    for j in range(0, l-i-1):
-      if nums[j] > nums[j + 1]:
-        nums[j], nums[j+1] = nums[j+1], nums[j]
-        swapped = True
-    if not swapped:
-      break
-  print(nums)
+  length = len(nums)
 
-bubbleSort([5,2,1,3,0])
+  for i in range(length):
+    # Initialize a flag to track if any swaps are made in this pass
+    swapped = False
+
+    # Iterate through the unsorted part of the list
+    for j in range(0, length - i - 1):
+      # Compare adjacent elements and swap if needed
+      if nums[j] > nums[j + 1]:
+        nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        swapped = True
+
+    # If no swaps were made in this pass, the list is already sorted
+    if not swapped:
+      # Without the swapped check, Bubble Sort would always go through the entire inner loop
+      # for each iteration of the outer loop, even if the list is already sorted.
+      # This would result in unnecessary comparisons and swaps.
+      break
+
+  print("Sorted Array:", nums)
+
+
+# Example usage
+bubbleSort([5, 2, 1, 3, 0])
